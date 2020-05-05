442b765406140410c39a6163d30636971ad22ff9
matthewparnham <parnham@chapman.edu>
2020-05-04 19:49:06 -0700

    working on reading kills from image.  Only working for some images!

--- ParseImg.py
+++ ParseImg.py
@@ -31,7 +31,7 @@ def classify_hero(im):
             return hero_image_names[i]
 
 
-img = cv2.imread('2020-05-01.png')
+img = cv2.imread('2020-05-01 (1).png')
 
 img = img[:,1920:,:]
 
@@ -43,25 +43,7 @@ dsize = (width, height)
 
 scaled_im = cv2.resize(img, dsize)
 
-"""
-curr_im = output[176:200,138:162,:]
 
-cv2.imwrite('hero_training/cypher2.png',curr_im)
-cv2.imshow('image',curr_im)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-#output[176:200,138:162,:]
-#output[202:226,138:162,:]
-#output[228:252,138:162,:]
-#output[254:278,138:162,:]
-#output[280:304,138:162,:]
-#output[306:330,138:162,:]
-#output[332:356,138:162,:]
-#output[358:382,138:162,:]
-#output[384:408,138:162,:]
-#output[410:434,138:162,:]
-"""
 hero_positions = [
     [176,200,138,162],
     [202,226,138,162],
@@ -74,14 +56,6 @@ hero_positions = [
     [384,408,138,162],
     [410,434,138,162]
                   ]
-
-match_heroes = []
-for f in range(len(hero_positions)):
-    curr_im = scaled_im[hero_positions[f][0]:hero_positions[f][1],138:162,:]
-    match_heroes.append(classify_hero(curr_im))
-
-
-
 
 player_name_positions = [
     [352, 400, 328, 600],
@@ -109,11 +83,34 @@ ACS_positions = [
     [820, 868, 650, 750]
                 ]
 
-match_player_names = []
-player = 0
-for p in range(len(player_name_positions)):
+kills_positions = [
+    [352, 400, 830, 870],
+    [404, 452, 830, 870],
+    [456, 504, 830, 870],
+    [508, 556, 830, 870],
+    [560, 608, 830, 870],
+    [612, 660, 830, 870],
+    [664, 712, 830, 870],
+    [716, 764, 830, 870],
+    [768, 816, 830, 870],
+    [820, 868, 830, 870]
+                ]
 
-    player = p
+
+match_heroes = []
+match_player_names = []
+match_ACS = []
+match_kills = []
+
+for i in range(10):
+    curr_im = scaled_im[hero_positions[i][0]:hero_positions[i][1],138:162,:]
+    match_heroes.append(classify_hero(curr_im))
+
+
+
+
+for i in range(10):
+    player = i
     img_slice = img[player_name_positions[player][0]:player_name_positions[player][1],player_name_positions[player][2]:player_name_positions[player][3],:]
     #img_slice = cv2.medianBlur(img_slice,3)
     img_slice = cv2.cvtColor(img_slice, cv2.COLOR_BGR2GRAY)
@@ -123,11 +120,10 @@ for p in range(len(player_name_positions)):
     match_player_names.append(text)
 
 
-match_ACS = []
-player = 0
-for p in range(len(ACS_positions)):
 
-    player = p
+
+for i in range(10):
+    player = i
     img_slice = img[ACS_positions[player][0]:ACS_positions[player][1],ACS_positions[player][2]:ACS_positions[player][3],:]
     img_slice = cv2.medianBlur(img_slice,3)
     img_slice = cv2.cvtColor(img_slice, cv2.COLOR_BGR2GRAY)
@@ -136,6 +132,23 @@ for p in range(len(ACS_positions)):
     text = pytesseract.image_to_string(img_slice)
     match_ACS.append(text)
 
-print("hero name acs")
-for q in range(10):
-    print(match_heroes[q],match_player_names[q],match_ACS[q])
+for i in range(10):
+    player = i
+    img_slice = img[kills_positions[player][0]:kills_positions[player][1],kills_positions[player][2]:kills_positions[player][3],:]
+    #img_slice = cv2.medianBlur(img_slice, 3)
+    img_slice = cv2.cvtColor(img_slice, cv2.COLOR_BGR2GRAY)
+    #img_slice = cv2.adaptiveThreshold(img_slice, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+    img_slice = cv2.bitwise_not(img_slice)
+    img_slice = cv2.medianBlur(img_slice, 3)
+    #img_slice = cv2.adaptiveThreshold(img_slice, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+
+    #cv2.imwrite('kills/{}.png'.format(i),img_slice)
+    text = pytesseract.image_to_string(img_slice)
+    if text == '':
+        text = 'error'
+    match_kills.append(text)
+
+
+print("hero name acs k")
+for i in range(10):
+    print(match_heroes[i],match_player_names[i],match_ACS[i],match_kills[i])
